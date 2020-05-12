5f386d92f5ab1255cca419437504371435bb0fc8
coding-fantastic <alexnzioka@protonmail.com>
2020-05-12 23:52:56 +0300

    modifiy post from admin page

--- blog/views.py
+++ blog/views.py
@@ -2,20 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
-posts = [
-{
-'author':'AlexMa',
-'title':'Blog Post 1',
-'content': 'First post content',
-'date_posted': 'August 27 , 2019'
-},
-{
-'author':'Jane',
-'title':'Blog Post 2',
-'content': 'Second post content',
-'date_posted': 'July 27 , 2020'
-}
-]
 
 # Create your views here.
 def home(request):
